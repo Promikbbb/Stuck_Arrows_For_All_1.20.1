eafb372ccda0cdcb7433c71ebd99c9ce8f9a557f
Promikbbb <romanovalexandr2009@mail.ru>
2026-04-01 22:57:40 +0900

    1.20.1

--- common/src/main/java/traben/entity_pin_cushions/mixin/MixinAddLayer.java
+++ common/src/main/java/traben/entity_pin_cushions/mixin/MixinAddLayer.java
@@ -4,7 +4,7 @@ import net.minecraft.client.model.*;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import traben.entity_pin_cushions.PinCushionLayer;
 
 @Mixin(value = LivingEntityRenderer.class,priority = 2000)
-public abstract class MixinAddLayer<T extends LivingEntityRenderState, M extends EntityModel<T>> {
+public abstract class MixinAddLayer<T extends LivingEntity, M extends EntityModel<T>> {
 
     @Shadow protected M model;
 
@@ -21,11 +21,12 @@ public abstract class MixinAddLayer<T extends LivingEntityRenderState, M extends
 
     @Inject(method = "<init>", at = @At(value = "TAIL"))
     private void allStuckArrows$mixin(final EntityRendererProvider.Context context, final EntityModel<?> model, final float shadowRadius, final CallbackInfo ci) {
-        if ( (this.model instanceof PiglinModel || !(this.model instanceof PlayerModel))){
+        if ((this.model instanceof AgeableListModel<?> || this.model instanceof HierarchicalModel<?>)
+                && (this.model instanceof PiglinModel<?> || !(this.model instanceof PlayerModel<?>))){
             try {
-                LivingEntityRenderer<?,T, M> self = (LivingEntityRenderer) (Object) this;
+                LivingEntityRenderer<T, M> self = (LivingEntityRenderer) (Object) this;
                 addLayer(new PinCushionLayer.ArrowLayer<>(context, self));
-                addLayer(new PinCushionLayer.BeeStingerLayer<>(context,self));
+                addLayer(new PinCushionLayer.BeeStingerLayer<>(self));
             } catch (Exception e) {
                 System.out.println("Failed to add custom stuck arrows layer to " + this.getClass().getName());
             }
